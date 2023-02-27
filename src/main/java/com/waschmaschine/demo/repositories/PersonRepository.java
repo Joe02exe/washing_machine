8baa693555b045d3c366ef7c501ff4fc35e532e9
Joe02exe <lonza.johannes@gmail.com>
2023-02-27 22:42:40 +0100

    Added Transaction annotation, because otherwise deleting would not be possible

--- src/main/java/com/waschmaschine/demo/repositories/PersonRepository.java
+++ src/main/java/com/waschmaschine/demo/repositories/PersonRepository.java
@@ -2,7 +2,9 @@ package com.waschmaschine.demo.repositories;
 
 import com.waschmaschine.demo.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface PersonRepository extends JpaRepository<Person, String> {
     Person findPersonByUsername(String username);
 
