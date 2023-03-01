763a43cf8a670f041917881ae7b38b451bab17df
Joe02exe <lonza.johannes@gmail.com>
2023-03-01 22:38:46 +0100

    fixed a few Bugs

--- src/main/java/com/waschmaschine/demo/services/PersonService.java
+++ src/main/java/com/waschmaschine/demo/services/PersonService.java
@@ -1,19 +1,30 @@
 package com.waschmaschine.demo.services;
 
+import com.waschmaschine.demo.configs.WebSecurityConfig;
+import com.waschmaschine.demo.Controller.CustomUserDetails;
 import com.waschmaschine.demo.model.Person;
+import com.waschmaschine.demo.model.UserRole;
 import com.waschmaschine.demo.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
-public class PersonService {
+public class PersonService implements UserDetailsService {
     @Autowired
     private PersonRepository personRepository;
 
-    public Person findPersonByUsername(String username){
-        return personRepository.findPersonByUsername(username);
+    public Person findPersonByUsername(String username) throws UsernameNotFoundException{
+        Person person = personRepository.findPersonByUsername(username);
+        if (person == null){
+            throw new UsernameNotFoundException("Username: " + username +" was not found");
+        }
+        return person;
     }
 
     public List<Person> findAllPersons(){
@@ -21,14 +32,25 @@ public class PersonService {
     }
 
     public Person addPerson(Person person){
+        person.setRole(UserRole.USER);
+        person.setPassword(WebSecurityConfig.passwordEncoder().encode(person.getPassword()));
         return personRepository.save(person);
     }
 
     public Person updatePerson(Person person){
+        if(!Objects.equals(findPersonByUsername(person.getUsername()).getPassword(), person.getUsername())){
+            person.setPassword(WebSecurityConfig.passwordEncoder().encode(person.getPassword()));
+        }
         return personRepository.save(person);
     }
 
     public void deleteEmployeeByUsername(String username) {
         personRepository.deletePersonByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Person person = findPersonByUsername(username);
+        return new CustomUserDetails(person);
     }
 }
