a4aeedaa6b878120faa8641b6aaab269a18cd766
Joe02exe <lonza.johannes@gmail.com>
2023-03-03 20:00:30 +0100

    added more event-methods for new fields

--- src/main/java/com/waschmaschine/demo/repositories/EventRepository.java
+++ src/main/java/com/waschmaschine/demo/repositories/EventRepository.java
@@ -2,8 +2,12 @@ package com.waschmaschine.demo.repositories;
 
 import com.waschmaschine.demo.model.Event;
 import com.waschmaschine.demo.model.Person;
+import com.waschmaschine.demo.model.WashingMachine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
@@ -12,4 +16,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Event findEventById(Long id);
 
     void deleteEventById(Long id);
+
+    @Query("SELECT d FROM Event d WHERE d.washingMachine = :washingMachine AND :time BETWEEN d.beginDate AND d.endDate")
+    Event getEventWithinTime(@Param("time")LocalDateTime time,@Param("washingMachine") WashingMachine washingMachine);
 }
--- src/main/java/com/waschmaschine/demo/services/EventService.java
+++ src/main/java/com/waschmaschine/demo/services/EventService.java
@@ -2,6 +2,7 @@ package com.waschmaschine.demo.services;
 
 import com.waschmaschine.demo.model.Event;
 import com.waschmaschine.demo.model.Person;
+import com.waschmaschine.demo.model.WashingMachine;
 import com.waschmaschine.demo.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,14 +35,26 @@ public class EventService {
 
     public List<Event> getNewestEvents(){
         List<Event> events =  getAllEvents().stream()
-                .filter(event -> event.getDate()
+                .filter(event -> event.getBeginDate()
                         .isAfter(LocalDateTime.now()))
-                .sorted(Comparator.comparing(Event::getDate))
+                .sorted(Comparator.comparing(Event::getBeginDate))
                 .toList();
         if (events.size() <=5){
             return events;
         }
         return events.subList(0,4);
+    }
+
+    public Event getEventWithinTime(LocalDateTime time, WashingMachine washingMachine){
+        return eventRepository.getEventWithinTime(time, washingMachine);
+    }
+
+    public String getEventString(LocalDateTime time, WashingMachine washingMachine){
+        Event event = getEventWithinTime(time, washingMachine);
+        if (event == null){
+            return "<frei>";
+        }
+        return event.getPerson().getUsername() + "(" + event.getPerson().getFloor() +")";
     }
 
     public Event addEvent(Event event){
