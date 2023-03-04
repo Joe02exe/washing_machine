3355a8f0ebf6812928c04d92941e98584a19e4fc
Joe02exe <lonza.johannes@gmail.com>
2023-03-04 09:06:14 +0100

    added Guest registration, still have to add auto login after registration, but solving this temporarily with displaying password for Guest user

--- src/main/java/com/waschmaschine/demo/Controller/LoginController.java
+++ src/main/java/com/waschmaschine/demo/Controller/LoginController.java
@@ -1,15 +1,49 @@
 package com.waschmaschine.demo.Controller;
 
+import com.waschmaschine.demo.configs.WebSecurityConfig;
+import com.waschmaschine.demo.model.Floor;
+import com.waschmaschine.demo.model.Person;
+import com.waschmaschine.demo.model.UserRole;
+import com.waschmaschine.demo.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("")
 public class LoginController {
 
-    @GetMapping
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("/guest")
+    public String guestLogin(Model model){
+        model.addAttribute("person", new Person());
+        return "guestLogin";
+    }
+
+    @PostMapping("/process_guest")
+    public String processRegistration(Person person, Model model){
+        person.setRole(UserRole.GUEST);
+        person.setPassword(WebSecurityConfig
+                .passwordEncoder()
+                .encode(String
+                        .valueOf(((int)(Math.floor(Math.random()*100000))))));
+        if(personService.findPersonByUsername(person.getUsername())!=null){
+            model.addAttribute("person", new Person());
+            return "registerFail";
+        }
+        System.out.println("Found2222!!!!!!");
+
+        personService.addPerson(person);
+        return "guestLoginSuccess";
     }
 }
